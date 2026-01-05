6749a52b6ceeec7c0e540efd30505c167892fc02
Christer Carlsson <ccarlsson@outlook.com>
2026-01-05 02:07:04 +0100

    Add XML docs to exceptions; bump version to 1.0.2
    Added XML documentation to HandlerNotFoundException and MediatorException for improved code clarity and API reference. Updated package version in SimpleMediator.csproj from 1.0.1 to 1.0.2 to reflect these documentation enhancements.

--- SimpleMediator/HandlerNotFoundException.cs
+++ SimpleMediator/HandlerNotFoundException.cs
@@ -7,6 +7,11 @@ namespace SimpleMediator;
 /// </summary>
 public sealed class HandlerNotFoundException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HandlerNotFoundException"/> class.
+    /// </summary>
+    /// <param name="requestType">The request type that was sent.</param>
+    /// <param name="responseType">The expected response type.</param>
     public HandlerNotFoundException(Type requestType, Type responseType)
         : base($"No handler was registered for request type '{requestType.FullName}' with response type '{responseType.FullName}'.")
     {
@@ -14,7 +19,13 @@ public sealed class HandlerNotFoundException : Exception
         ResponseType = responseType;
     }
 
+    /// <summary>
+    /// Gets the request type that had no handler registered.
+    /// </summary>
     public Type RequestType { get; }
 
+    /// <summary>
+    /// Gets the response type associated with the request.
+    /// </summary>
     public Type ResponseType { get; }
 }
--- SimpleMediator/MediatorException.cs
+++ SimpleMediator/MediatorException.cs
@@ -6,14 +6,27 @@ namespace SimpleMediator;
 /// </summary>
 public sealed class MediatorException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MediatorException"/> class with a specified error message
+    /// and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">The exception that caused the current exception.</param>
     public MediatorException(string message, Exception innerException) : base(message, innerException)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MediatorException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public MediatorException(string message) : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MediatorException"/> class.
+    /// </summary>
     public MediatorException()
     {
     }
